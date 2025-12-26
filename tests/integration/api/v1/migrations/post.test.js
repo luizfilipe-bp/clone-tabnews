ba7653a1c23e7db8f9c409921c4bbe93c36b7c72
Luiz Filipe Bartelega Penha <luizfpenha16@gmail.com>
2025-12-26 10:36:38 -0300

    feat: add endpoint api/v1/migrations

---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,31 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
+test("GET to /api/v1/migrations should return status 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response.status).toBe(201);
+
+  let responseBody = await response.json();
+
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
+
+  const secondResponse = await fetch(
+    "http://localhost:3000/api/v1/migrations",
+    {
+      method: "POST",
+    },
+  );
+  expect(secondResponse.status).toBe(200);
+
+  responseBody = await secondResponse.json();
+
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBe(0);
+});
