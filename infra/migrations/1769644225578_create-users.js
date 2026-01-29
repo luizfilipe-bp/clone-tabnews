7f7cf6665dc342ad6c9532e94213b2b18144feaa
Luiz Filipe Bartelega Penha <luizfpenha16@gmail.com>
2026-01-28 21:31:23 -0300

    feat: add create_user migration

---
+++ infra/migrations/1769644225578_create-users.js
@@ -0,0 +1,36 @@
+exports.up = (pgm) => {
+  pgm.createTable("users", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()"),
+    },
+    username: {
+      type: "varchar(30)",
+      notNull: true,
+      unique: true,
+    },
+    email: {
+      type: "varchar(254)",
+      notNull: true,
+      unique: true,
+    },
+    // max length for bcrypt hashed passwords
+    password: {
+      type: "varchar(60)",
+      notNull: true,
+    },
+    created_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
+    },
+    updated_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
+    },
+  });
+};
+
+exports.down = false;
