a94f6fae47a0103533a3a3d546e3d57dfc6f2d83
Luiz Filipe Bartelega Penha <luizfpenha16@gmail.com>
2026-03-18 22:48:38 -0300

    feat: create session endpoint and test

--- models/user.js
+++ models/user.js
@@ -2,6 +2,37 @@ import database from "infra/database";
 import { NotFoundError, ValidationError } from "infra/errors";
 import password from "models/password.js";
 
+async function findOneByEmail(email) {
+  const userFound = await runSelectQuery(email);
+
+  return userFound;
+
+  async function runSelectQuery(email) {
+    const results = await database.query({
+      text: `
+        SELECT 
+          *
+        FROM
+          users
+        WHERE
+          LOWER(email) = LOWER($1)
+        LIMIT
+          1
+      ;`,
+      values: [email],
+    });
+
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O email informado não foi encontrado no sistema.",
+        action: "Verifique se o email está digitado corretamente.",
+      });
+    }
+
+    return results.rows[0];
+  }
+}
+
 async function findOneByUsername(username) {
   const userFound = await runSelectQuery(username);
 
@@ -164,6 +195,7 @@ async function hashPasswordInObject(userInputValues) {
 
 const user = {
   create,
+  findOneByEmail,
   findOneByUsername,
   update,
 };
