4960f1a5d05290a373dabb5fd2e5daf37f11511d
Luiz Filipe Bartelega Penha <luizfpenha16@gmail.com>
2026-06-03 23:18:51 -0300

    feat: implements local infra to send emails

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -5,8 +5,12 @@ import database from "infra/database";
 import migrator from "models/migrator";
 import user from "models/user";
 import session from "models/session";
+
+const emailHttpUrl = `http://${process.env.EMAIL_HTTP_HOST}:${process.env.EMAIL_HTTP_PORT}`;
+
 async function waitForAllServices() {
   await waitForWebServer();
+  await waitForEmailServer();
 
   async function waitForWebServer() {
     return retry(fetchStatusPage, {
@@ -16,6 +20,21 @@ async function waitForAllServices() {
 
     async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
+
+      if (response.status !== 200) {
+        throw Error();
+      }
+    }
+  }
+
+  async function waitForEmailServer() {
+    return retry(fetchEmailStatus, {
+      retries: 100,
+      maxTimeout: 1000,
+    });
+
+    async function fetchEmailStatus() {
+      const response = await fetch(emailHttpUrl);
 
       if (response.status !== 200) {
         throw Error();
@@ -45,12 +64,36 @@ async function createSession(userId) {
   return await session.create(userId);
 }
 
+async function getLastEmail() {
+  const emailListResponse = await fetch(`${emailHttpUrl}/messages`);
+
+  const emailListBody = await emailListResponse.json();
+  const lastEmailItem = emailListBody.pop();
+
+  const lastEmailTextResponse = await fetch(
+    `${emailHttpUrl}/messages/${lastEmailItem.id}.plain`,
+  );
+
+  const lastEmailTextBody = await lastEmailTextResponse.text();
+  lastEmailItem.text = lastEmailTextBody;
+
+  return lastEmailItem;
+}
+
+async function deleteAllEmails() {
+  await fetch(`${emailHttpUrl}/messages`, {
+    method: "DELETE",
+  });
+}
+
 const orchestrator = {
   waitForAllServices,
   clearDatabase,
   runPendingMigrations,
   createUser,
   createSession,
+  getLastEmail,
+  deleteAllEmails,
 };
 
 export default orchestrator;
