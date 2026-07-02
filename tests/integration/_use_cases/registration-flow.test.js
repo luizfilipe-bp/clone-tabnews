5b6896a2d9bcbdae51e4b38b1425b1bc22407994
Luiz Filipe Bartelega Penha <luizfpenha16@gmail.com>
2026-07-01 23:40:24 -0300

    feat: creates injectAnonymousOrUser and canRequest middlewares

--- tests/integration/_use_cases/registration-flow.test.js
+++ tests/integration/_use_cases/registration-flow.test.js
@@ -85,7 +85,27 @@ describe("Use case: Registration Flow (all successful)", () => {
     expect(activatedUser.features).toEqual(["create:session"]);
   });
 
-  test("Login with activated account", async () => {});
+  test("Login", async () => {
+    const createSessionResponse = await fetch(
+      "http://localhost:3000/api/v1/sessions",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: "registrationflow@example.com",
+          password: "password123",
+        }),
+      },
+    );
+
+    expect(createSessionResponse.status).toBe(201);
+
+    const createSessionResponseBody = await createSessionResponse.json();
+
+    expect(createSessionResponseBody.user_id).toBe(createUserResponseBody.id);
+  });
 
   test("Get user information", async () => {});
 });
