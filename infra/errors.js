ad8e7b8a2b68ddd84d3f00c46bd5309e6d121734
Luiz Filipe Bartelega Penha <luizfpenha16@gmail.com>
2026-07-08 22:28:58 -0300

    refactor: improve error loging in `email.send`

--- infra/errors.js
+++ infra/errors.js
@@ -38,13 +38,14 @@ export class MethodNotAllowedError extends Error {
 }
 
 export class ServiceError extends Error {
-  constructor({ cause, message }) {
+  constructor({ cause, message, action, context }) {
     super(message || "Serviço indisponível no momento.", {
       cause,
     });
     this.name = "ServiceError";
-    this.action = "Verifique se o serviço está disponível.";
+    this.action = action || "Verifique se o serviço está disponível.";
     this.statusCode = 503;
+    this.context = context;
   }
 
   toJSON() {
@@ -53,6 +54,7 @@ export class ServiceError extends Error {
       message: this.message,
       action: this.action,
       status_code: this.statusCode,
+      context: this.context,
     };
   }
 }
